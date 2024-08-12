ba8c8a22e676a2f2168f9e2a3489468cfeb3ee68
AriRainbow <mariel.williams@atlasschool.com>
2024-08-12 23:18:41 +0000

    Change to use malloc, previously thought I couldn't

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,8 +1,6 @@
 #include "binary_trees.h"
 #include <stddef.h>
 
-#define MAX_NODES 1024
-
 /**
  * binary_tree_node - Creates binary tree node
  * @parent: Pointer to parent node
@@ -12,14 +10,11 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	static binary_tree_t nodes[MAX_NODES];
-	static size_t next_node_index = 0;
 	binary_tree_t *new_node;
 
-	if (next_node_index >= MAX_NODES)
-		return (NULL); /* no space available */
-
-	new_node = &nodes[next_node_index++]; /* get next available node */
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL); /* allocation failed */
 
 	/* initialize new node */
 	new_node->n = value;
