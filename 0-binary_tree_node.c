32210c201fc4932bca1398182b08f04ab879dfe7
AriRainbow <mariel.williams@atlasschool.com>
2024-08-12 21:54:30 +0000

    0. New node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+#include <stddef.h>
+
+#define MAX_NODES 1024
+
+/**
+ * binary_tree_node - Creates binary tree node
+ * @parent: Pointer to parent node
+ * @value: Value to put in new node
+ *
+ * Return: Pointer to new node, or NULL if no space available
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	static binary_tree_t nodes[MAX_NODES];
+	static size_t next_node_index = 0;
+	binary_tree_t *new_node;
+
+	if (next_node_index >= MAX_NODES)
+		return (NULL);
+
+	new_node = &nodes[next_node_index++];
+
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
+}
