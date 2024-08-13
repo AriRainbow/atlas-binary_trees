87c6707f06b852f146c5b96817f33016ce406f68
AriRainbow <mariel.williams@atlasschool.com>
2024-08-13 01:43:31 +0000

    9. Height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measures height of a binary tree
+ * @tree: Pointer to root node of tree to measure height
+ *
+ * Return: Height of tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	/* Measure height of left subtree */
+	left_height = binary_tree_height(tree->left);
+
+	/* Measure height of right subtree */
+	right_height = binary_tree_height(tree->right);
+
+	/* height of tree is the greater of 2 subtree heights +1 for root */
+	return ((left_height > right_height ? left_height : right_height) + 1);
+}
