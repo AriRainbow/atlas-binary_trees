a65b6d10cae49e765537de2be87e4fd6b2fdd4e1
AriRainbow <mariel.williams@atlasschool.com>
2024-08-13 03:59:13 +0000

    warning: no description found for function binary_tree_sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -4,6 +4,10 @@
  * binary_tree_siblings - Finds the sibling of a node
  * @node: Pointer to the node to find the sibling of
  *
+ * Description: Function checks the parent of the given node
+ * It then returns the sibling node of the given node
+ * If the node doesn't have a sibling or parent is NULL, returns NULL
+ *
  * Return: Pointer to the sibling node, or NULL if no sibling
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
