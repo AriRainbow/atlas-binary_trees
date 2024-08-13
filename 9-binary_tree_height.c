8b41b644dbe31055f2795cce2b6c78545a22a7c5
AriRainbow <mariel.williams@atlasschool.com>
2024-08-13 01:59:17 +0000

    Correct output - case: Single node

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,12 +14,15 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	/* Measure height of left subtree */
-	left_height = binary_tree_height(tree->left);
+	/* if node has no children, height is 0 */
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
 
-	/* Measure height of right subtree */
-	right_height = binary_tree_height(tree->right);
+	if (tree->left != NULL)
+		left_height = binary_tree_height(tree->left) + 1;
 
-	/* height of tree is the greater of 2 subtree heights +1 */
-	return ((left_height > right_height ? left_height : right_height) + 1);
+	if (tree->right != NULL)
+		right_height = binary_tree_height(tree->right) + 1;
+
+	return (left_height > right_height ? left_height : right_height);
 }
